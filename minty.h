cfe985e33a59d217424c04764c51f058db3f3f64
Musakalamz <ogunsolumusa@gmail.com>
2023-10-19 14:55:07 -0700

    Monty Language

--- minty.h
+++ minty.h
@@ -13,33 +13,33 @@
 extern char **op_toks;
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
+ *  * struct stack_s - doubly linked list representation of a stack (or queue)
+ *   * @n: integer
+ *    * @prev: points to the previous element of the stack (or queue)
+ *     * @next: points to the next element of the stack (or queue)
+ *      *
+ *       * Description: doubly linked list node structure
+ *        * for stack, queues, LIFO, FIFO Holberton project
+ *         */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+		int n;
+			struct stack_s *prev;
+				struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
+ *  * struct instruction_s - opcode and its function
+ *   * @opcode: the opcode
+ *    * @f: function to handle the opcode
+ *     *
+ *      * Description: opcode and its function
+ *       * for stack, queues, LIFO, FIFO Holberton project
+ *        */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+			void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* PRIMARY INTERPRETER FUNCTIONS */
@@ -85,14 +85,5 @@ int pint_error(unsigned int line_number);
 int short_stack_error(unsigned int line_number, char *op);
 int div_error(unsigned int line_number);
 int pchar_error(unsigned int line_number, char *message);
-
-/* Main Helpers */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-
-
-/* String functions */
-char *_strcpy(char *dest, const char *src);
 
 #endif /* __MONTY_H__ */
